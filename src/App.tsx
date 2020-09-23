3957abaa4cd32bbf3b29cb2ffebdbf8e06d63abf
IzzyBeraja <20325290+IzzyBeraja@users.noreply.github.com>
2020-09-22 23:43:39 -0400

    Added my first line of Typescript! 😁

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+    const name: string = "Hello World!";
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Edit <code>{name}</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
