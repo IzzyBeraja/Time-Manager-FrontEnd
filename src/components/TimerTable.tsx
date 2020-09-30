ce8ffffdbc0704c856122422624f0d46bcf8f1cb
IzzyBeraja <20325290+IzzyBeraja@users.noreply.github.com>
2020-09-30 02:55:48 -0400

    Removing files no longer in use 🙋🏻‍♂️

--- src/components/TimerTable.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Table from "./common/Table";
-import { getTasks } from "../services/FakeTaskService";
-
-const TimerTable: React.FC = () => {
-  return <Table tasks={getTasks()} />;
-};
-
-export default TimerTable;
--- src/components/common/TableHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {
-  text: string;
-}
-
-export const TableHeader: React.FC<Props> = props => {
-  return <h1>{props.text}</h1>;
-};
