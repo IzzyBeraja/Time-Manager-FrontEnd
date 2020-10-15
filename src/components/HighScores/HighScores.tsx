d983a24e61049c8563fea44dbb53bf738c241637
Izzy Beraja <izzyberaja+github@gmail.com>
2020-10-15 15:38:31 -0400

    Minor formatting changes

--- src/components/HighScores/HighScores.tsx
+++ src/components/HighScores/HighScores.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HighScores = () => {
-  return <h1>High Scores</h1>;
+  return <h1 className="container">High Scores</h1>;
 };
 
 export default HighScores;
--- src/components/KeyLayouts/KeyLayouts.tsx
+++ src/components/KeyLayouts/KeyLayouts.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const KeyLayouts = () => {
-  return <h1>KeyLayouts</h1>;
+  return <h1 className="container">KeyLayouts</h1>;
 };
 
 export default KeyLayouts;
